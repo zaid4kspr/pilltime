a545d4c029aede44cb8b78cd6f5f5cf073f8dccf
Zaid Selmi <selmizaid@gmail.com>
2020-04-25 00:11:50 +0100

    first commit

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int splashTime=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i =new Intent(SplashScreen.this,register.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTime);
+
+
+
+
+
+    }
+
+    public void run(){
+        Intent i =new Intent(SplashScreen.this,register.class);
+        startActivity(i);
+        finish();
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/register.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        setContentView(R.layout.activity_register);
+    }
+}
