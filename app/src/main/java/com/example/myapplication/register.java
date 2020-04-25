e0cb5019c04a60f5fe1c546910f3fca9b6104513
Zaid Selmi <selmizaid@gmail.com>
2020-04-25 02:26:43 +0100

    z

--- app/src/main/java/com/example/myapplication/register.java
+++ app/src/main/java/com/example/myapplication/register.java
@@ -4,14 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_register);
+
+        final Button button = (Button) findViewById(R.id.registerBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your handler code here
+                routeToMain();
+            }
+        });
+
+
+
+    }
+
+
+
+
+    public void routeToMain(){
+
+    Intent i = new Intent(register.this,addMed.class);
+    startActivity(i);
+
     }
 }
