012f9862f1e268668350513d5965454dfcd1b8fb
Zaid Selmi <selmizaid@gmail.com>
2020-05-15 03:55:21 +0100

    home page with dynamic calendar

--- app/src/main/java/com/example/myapplication/med_form.java
+++ app/src/main/java/com/example/myapplication/med_form.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 import android.os.Bundle;
@@ -15,12 +19,23 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.ui.DatePickerFragment;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnFocusChange;
 
-public class med_form extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class med_form extends AppCompatActivity implements DatePickerDialog.OnDateSetListener , AdapterView.OnItemSelectedListener  {
+
+    @BindView(R.id.dateDebut) TextInputLayout  dateDebut ;
 
 
     @Override
@@ -28,6 +43,7 @@ public class med_form extends AppCompatActivity implements AdapterView.OnItemSel
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_med_form);
+
         LinearLayout linearLayout = (LinearLayout)findViewById(R.id.dialTake);
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +55,8 @@ public class med_form extends AppCompatActivity implements AdapterView.OnItemSel
                 dialog.show();
             }
         });
+
+        ButterKnife.bind(this);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -55,7 +73,32 @@ public class med_form extends AppCompatActivity implements AdapterView.OnItemSel
                 Intent i =new Intent(med_form.this, activity_list_med.class);
                 startActivity(i);            }
         });
+
+
+
+   
+
+
+
     }
+    @OnFocusChange(R.id.dateDebutInput)
+    public void openCalender( View view, boolean hasFocus){
+        if (hasFocus)
+        {
+            DialogFragment datePicker = new DatePickerFragment();
+            datePicker.show(getSupportFragmentManager(), "date pickerr");
+        }
+
+    }
+    @OnClick(R.id.dateDebutInput)
+    public void openCalenderr( View view){
+
+            DialogFragment datePicker = new DatePickerFragment();
+            datePicker.show(getSupportFragmentManager(), "date pickerr");
+
+
+    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -67,6 +110,33 @@ public class med_form extends AppCompatActivity implements AdapterView.OnItemSel
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)  {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+
+
+
+
+        String currentDateString = dateFormat.format(c.getTime());
+
+
+
+dateDebut.getEditText().setText(currentDateString);
+
+
+
+
+
+
+    }
+
+
 
 
 }
